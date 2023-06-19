b25bf65d334a54994f58e49fe24615a0bf4268db
tatitani <tatitani@naver.com>
2023-06-19 17:05:29 +0900

    [feat] 유틸 클래스 추가 - 간단히 에셋 로드

---
+++ Source/Playground/Utils/UtilPlayground.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class PLAYGROUND_API UtilPlayground
+{
+public:
+	UtilPlayground();
+	~UtilPlayground();
+
+	template <typename T>
+	static T* LoadAsset(FString FileName);
+	
+	
+};
+
+template <typename T>
+T* UtilPlayground::LoadAsset(FString FileName)
+{
+	static ConstructorHelpers::FObjectFinder<T> MyAsset((TEXT("%s"), *FileName));
+	return MyAsset.Object;
+}
+
+
